7c5812cd283790fdeffad38713043fbbbf83d382
Caleb Madrigal <caleb.madrigal@fireeye.com>
2015-05-22 18:07:45 -0500

    Parallel http request example

---
+++ parallel_http_get.py
@@ -0,0 +1,33 @@
+""" Downloads a few web pages in parallel, and counts how many times a word is used in 
+all of them. """
+
+import asyncio
+import aiohttp
+
+@asyncio.coroutine
+def download_and_count_word(word, url):
+    response = yield from aiohttp.request('GET', url)
+    text = yield from response.read()
+    return text.decode().count(word)
+
+@asyncio.coroutine
+def count_word_in_pages(word, urls):
+    tasks = [download_and_count_word(word, url) for url in urls]
+    counts = yield from asyncio.gather(*tasks)
+
+    for i in range(len(urls)):
+        url = urls[i]
+        count = counts[i]
+        print("{} appears {} times in {}".format(word, count, url))
+
+word = "the"
+pages = ["http://calebmadrigal.com",
+         "http://yahoo.com",
+         "http://xkcd.com",
+         "http://reddit.com",
+         "http://news.ycombinator.com"]
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(count_word_in_pages(word, pages))
+loop.close()
+
