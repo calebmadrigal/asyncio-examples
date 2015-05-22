f1fada14b86160f8589f74bc889ffffb07aca3b7
Caleb Madrigal <caleb.madrigal@fireeye.com>
2015-05-22 10:27:04 -0500

    Added code to stop the event loop.

--- tail_recursion_with_asyncio.py
+++ tail_recursion_with_asyncio.py
@@ -4,10 +4,14 @@ import asyncio
 # keep the stack from growing.
 @asyncio.coroutine
 def factorial(n, acc=1):
-    if n == 0: print(acc)
-    else: asyncio.async(factorial(n-1, acc*n)) # async -> ensure_future in Python 3.4.4
+    if n == 0:
+        print(acc)
+        loop = asyncio.get_event_loop()
+        loop.stop()
+    else:
+        asyncio.async(factorial(n-1, acc*n)) # async -> ensure_future in Python 3.4.4
 
 loop = asyncio.get_event_loop()
 asyncio.async(factorial(1000))
-loop.run_forever()
+loop.run_forever() # Blocking call interrupted by loop.stop()
 
